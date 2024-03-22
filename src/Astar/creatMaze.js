add4544c3d50c76eae6e69bd6a4205561deb3887
rodionrybko <rybkorodion201@mail.ru>
2024-03-22 23:07:33 +0700

    creat Maze

---
+++ src/Astar/creatMaze.js
@@ -0,0 +1,18 @@
+const height = 5;
+const width = 5;
+
+function creatMaze(height, width){
+    let matrix = [];
+    for (i = 0; i < height; i++){
+        arr[i] = [];
+        for (j = 0; j < width; j++){
+            arr[i][j] = 0;
+        }
+    }
+    let x = Math.floor(Math.random() * (width / 2 - 0)) * 2 + 1;
+    let y = Math.floor(Math.random() * (height / 2 - 0)) * 2 + 1;
+    matrix[width][height] = 1;
+
+    let array = [];
+
+}
