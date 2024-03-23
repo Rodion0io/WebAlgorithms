c90725760c2ecc03e1e608e173b854f46af52095
rodionrybko <rybkorodion201@mail.ru>
2024-03-23 20:17:10 +0700

    Вроде починил генератор

--- src/Astar/creatMaze.js
+++ src/Astar/creatMaze.js
@@ -1,19 +1,19 @@
 const height = 5;
 const width = 5;
 
-function creatMaze(height, width){
-    let matrix = [];
-    for (i = 0; i < height; i++){
+function createMaze(height, width) {
+    var matrix = [];
+    for (let i = 0; i < height; i++){
         matrix[i] = [];
-        for (j = 0; j < width; j++){
+        for (let j = 0; j < width; j++){
             matrix[i][j] = 0;
         }
     }
-    let x = Math.floor(Math.random() * (width / 2)) * 2 + 1;
-    let y = Math.floor(Math.random() * (height / 2)) * 2 + 1;
+    var x = Math.floor(Math.random() * (width / 2)) * 2 + 1;
+    var y = Math.floor(Math.random() * (height / 2)) * 2 + 1;
     matrix[x][y] = 1;
 
-    let array = [];
+    var array = [];
     if (y - 2 >= 0){
         array.push({x: x, y: y - 2});
     }
@@ -28,45 +28,47 @@ function creatMaze(height, width){
     }
 
     while (array.length > 0){
-        const index = Math.floor(Math.random() * (array.length));
-        const cell = array[index];
+        var index = Math.floor(Math.random() * (array.length));
+        var cell = array[index];
         x = cell.x;
         y = cell.y;
-        matrix[x][y] = 10;
-        matrix.splice(index,1);
+        matrix[x][y] = 1;
+        array.splice(index,1);
     }
 
-    let directions = ['north', 'south', 'east', 'west'];
+    var directions = ['north', 'south', 'east', 'west'];
     while (directions.length > 0){
-        let directionsIndex = Math.floor(Math.random() * (directions.length));
+        var directionsIndex = Math.floor(Math.random() * (directions.length));
         switch (directions[directionsIndex]){
             case 'north':
-                if (y - 2 >= 0 && matrix[x][y - 2] == 10){
-                    matrix[x][y - 1] = 10;
+                if (y - 2 >= 0 && matrix[x][y - 2] == 1){
+                    matrix[x][y - 1] = 1;
                     directions = [];
                 }
                 break;
             case 'south':
-                if (y + 2 >= 0 && matrix[x][y + 2] == 10){
-                    matrix[x][y + 1] = 10;
+                if (y + 2 < height && matrix[x][y + 2] == 1){
+                    matrix[x][y + 1] = 1;
                     directions = [];
                 }
                 break;
             case 'east':
-                if (x - 2 >= 0 && matrix[x - 2][y] == 10){
-                    matrix[x - 1][y] = 10;
+                if (x + 2 < width && matrix[x + 2][y] == 1){
+                    matrix[x + 1][y] = 1;
                     directions = [];
                 }
                 break;
             case 'west':
-                if (x + 2 >= 0 && matrix[x + 2][y] == 10){
-                    matrix[x + 1][y] = 10;
+                if (x - 2 >= 0 && matrix[x - 2][y] == 1){
+                    matrix[x - 1][y] = 1;
                     directions = [];
                 }
                 break;
         }
         directions.splice(directionsIndex,1);
     }
+
+    var to_check = [];
     if (y - 2 >= 0 && matrix[x][y - 2] == 0){
         to_check.push({ x: x, y: y - 2 });
     }
@@ -82,3 +84,11 @@ function creatMaze(height, width){
     return matrix;
 }
 
+var matrix = createMaze(height, width);
+
+for (i = 0; i < height; i++){
+    console.log(matrix[i])
+    for (j = 0; j < width; j++){
+        console.log(matrix[i][j]);
+    }
+}
