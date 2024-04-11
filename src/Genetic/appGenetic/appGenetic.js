d8b595a7328a4754af00a16efed16d6e350f6442
rodionrybko <rybkorodion201@mail.ru>
2024-04-11 20:21:12 +0700

    add calculate distance and mutation

--- src/Genetic/appGenetic/appGenetic.js
+++ src/Genetic/appGenetic/appGenetic.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('genetic-field');
 const context = canvas.getContext('2d');
-let pointsArray = [];
+let pointsArray = []; // {x:x, y:y}
 const population = 100;
-const mutationRatio = 0.5;
+let generationGenetic = 1000;
 
 
 // Добавление точек в канвасе
@@ -45,26 +45,30 @@ document.getElementById('deleteLastButton').addEventListener('click',
     }
 );
 
+// Функция, которая высчитывает расстояние между точками
+function distBetwwenTwoPoints(firstPoint, secondPoint){
+    let distance = Math.sqrt(Math.pow((secondPoint.x - firstPoint.x), 2) + Math.pow((secondPoint.y - secondPoint.x), 2));
+    return distance;
+}
 
-// let arr = [1,2,3];
-
-// console.log(arr);
-
-// let res = arr.pop() * 10;
-
-// console.log(arr);
-// console.log(res);
+// Функция, которая считает длину пути(особи)
+function calculationWay(pointsArray){
+    let summWay = 0;
+    for (let i = 0; i < pointsArray.length - 1; i++){
+        summWay += distBetwwenTwoPoints(pointsArray[i], pointsArray[i + 1]);
+    }
+    return summWay;
+}
 
 // функция генерации особи
 function generateIndividum(pointsArray){
-    let individum = pointsArray.slice(); // Создаем копию массива pointsArray
+    let individum = pointsArray.slice();
     for (let i = individum.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
-        [individum[i], individum[j]] = [individum[j], individum[i]]; // Обмен элементов массива для перемешивания
+        [individum[i], individum[j]] = [individum[j], individum[i]];
     }
     return individum;
 }
-
 
 // Функция создания популяции 
 function creatPopulation(sizePopulation, pointsArray){
@@ -73,6 +77,17 @@ function creatPopulation(sizePopulation, pointsArray){
         population.push(generateIndividum(pointsArray));
     }
     return population;
+}
+
+// Функция, которая выбирает двух родителей
+function chooseParents(population){
+    let firstParent = population[Math.floor(Math.random() * population.length)];
+    let secondParent = population[Math.floor(Math.random() * population.length)];
+    if (firstParent == secondParent){
+        secondParent = population[Math.floor(Math.random() * population.length)];
+    }
+    
+    return [firstParent, secondParent];
 }
 
 // Функция, которая будет рандомно выбирать двух родителей для дальнейшего размножения
@@ -86,27 +101,27 @@ function crossing(firstParent, secondParent, sizePopulation) {
     let secondPartOfSecondChild = [];
     
     for (let i = breakPoint; i < secondParent.length; i++) {
-        if (!usedPointFirst.includes(secondParent[i])) {
+        if (!usedPointFirst.includes(secondParent[i])){
             secondPartOfFirstChild.push(secondParent[i]);
             usedPointFirst.push(secondParent[i]);
         }
     }
 
-    for (let i = breakPoint; i < firstParent.length; i++) {
-        if (!usedPointSecond.includes(firstParent[i])) {
+    for (let i = breakPoint; i < firstParent.length; i++){
+        if (!usedPointSecond.includes(firstParent[i])){
             secondPartOfSecondChild.push(firstParent[i]);
             usedPointSecond.push(firstParent[i]);
         }
     }
 
-    for (let i = breakPoint; i < firstParent.length; i++) {
+    for (let i = breakPoint; i < firstParent.length; i++){
         if (!usedPointFirst.includes(firstParent[i]) && secondPartOfFirstChild.length + firstPartOfFirstChild.length + 1 <= firstParent.length) {
             secondPartOfFirstChild.push(firstParent[i]);
             usedPointFirst.push(firstParent[i]);
         }
     }
 
-    for (let i = breakPoint; i < secondParent.length; i++) {
+    for (let i = breakPoint; i < secondParent.length; i++){
         if (!usedPointSecond.includes(secondParent[i]) && secondPartOfSecondChild.length + firstPartOfSecondChild.length + 1 <= secondParent.length) {
             secondPartOfSecondChild.push(secondParent[i]);
             usedPointSecond.push(secondParent[i]);
@@ -120,24 +135,24 @@ function crossing(firstParent, secondParent, sizePopulation) {
 }
 
 // Функция, которая будет проводить мутацию
-function mutate(individual, mutationRatio) {
-    if (Math.random() < mutationRatio) {
-      const index1 = Math.floor(Math.random() * individual.length);
-      let index2 = Math.floor(Math.random() * individual.length);
-      while (index1 === index2) {
-        index2 = Math.floor(Math.random() * individual.length);
-      }
-      [individual[index1], individual[index2]] = [individual[index2], individual[index1]];
+function mutate(individum, mutationRatio){
+    const mutationRatio = Math.floor(Math.random() * 101);
+    if (Math.random() < mutationRatio){
+        let firstIndex = Math.floor(Math.random() * individum.length);
+        let secondIndex = Math.floor(Math.random() * individum.length);
+        if (firstIndex == secondIndex){
+            secondIndex = Math.floor(Math.random() * individum.length);
+        }
+        let temp = firstIndex;
+        firstIndex = secondIndex;
+        secondIndex = temp;
     }
-  }
+}
 
-function geneticAlgorithm(pointsArray){
+// Сам генетический алгоритм
+function geneticAlgorithm(pointsArray, mutationRatio, generationGenetic){
 
 }
 
 document.getElementById('startGenetic').addEventListener('click', function(){
-    let a = [0,3,1,4,2];
-    let b = [0,2,4,3,1];
-    let res = crossing(a,b ,5);
-    console.log(res);
 })
